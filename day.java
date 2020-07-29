2756ff2a3dd7bc674dab66130110a597990fc507
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-07-29 07:03:56 -0700

    Add files via upload

---
+++ day.java
@@ -0,0 +1,24 @@
+class day
+{
+public static void main(String hj[])
+{
+int a=9;
+if(a==1)
+System.out.print("Monday");
+else if(a==2)
+System.out.print("Tuesday");
+else if(a==3)
+System.out.print("Wednesday");
+else if(a==4)
+System.out.print("Thuesday");
+else if(a==5)
+System.out.print("Friday");
+else if(a==6)
+System.out.print("Saturday");
+else if(a==7)
+System.out.print("Sunday");
+else
+System.out.print("This is Wrong No.");
+}
+}
+
