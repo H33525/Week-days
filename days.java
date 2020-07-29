efef5e970e556079830a2553b816e0942e035a7c
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-07-29 07:05:58 -0700

    Add files via upload

---
+++ days.java
@@ -0,0 +1,51 @@
+class days
+{
+public static void main(String hj[])
+{
+int a=1;
+switch(a)
+{
+case 1:
+{
+System.out.print("Monday");
+break;
+}
+case 2:
+{
+	System.out.print("Tuesday");
+	break;
+}
+case 3:
+{
+	System.out.print("Wednesday");
+	break;
+}
+case 4:
+{
+	System.out.print("Thuesday");
+	break;
+}
+case 5:
+{
+	System.out.print("Friday");
+	break;
+}
+case 6:
+{
+	System.out.print("Saturday");
+	break;
+}
+case 7:
+{
+	System.out.print("Sunday");
+	break;
+}
+default:
+{
+	System.out.print("Wrong Number");
+	break;
+}
+}
+}
+}
+
